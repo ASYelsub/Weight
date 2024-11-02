173959bc53699fab78a3934d82ceebd569e4737c
Krayno <alexanderpratt@gmail.com>
2024-11-02 21:20:51 +1300

    Commiting the whole project again with only the necessary files.

---
+++ Source/Weight/Weight.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class Weight : ModuleRules
+{
+	public Weight(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	}
+}
